c282c84c939b3cdb45edd9f2a13f3738948bd2ab
KyleenH <khunter@byupathway.edu>
2026-07-04 13:23:04 -0400

    modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,1 @@
-const year = document.querySelector("#year");
-const today = new Date();
-year.innerHTML = 'getFullYear(); <span class="highlight">${today.getFullYear()}</span>';
 document.getElementById("lastModified").innerHTML = document.lastModified;
