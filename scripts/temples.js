9cf72b9ca1660e2cb351c4f9d65668153290f901
KyleenH <khunter@byupathway.edu>
2026-07-12 00:56:01 -0400

    I hate this class

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,6 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
-hamButton.addEventListener('click', () =>{
+hambutton.addEventListener('click', () =>{
     mainnav.classList.toggle('show');
     hamButton.classList.toggle('show');
 });
