249e6e6dd0072751942bff3f34c6ada2345c484e
KyleenH <khunter@byupathway.edu>
2026-07-25 10:11:58 -0400

    week 4 assignment

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,8 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hamButton = document.querySelector('#menu');
+hamButton.addEventListener('click', () =>{
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
