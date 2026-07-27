8015e3f6d2ad5ab012021182bfbce11b794e5ce4
KyleenH <khunter@byupathway.edu>
2026-07-27 19:39:24 -0400

    update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -6,3 +6,113 @@ hamButton.addEventListener('click', () =>{
     mainnav.classList.toggle('show');
     hamButton.classList.toggle('show');
 });
+
+const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Manti Utah",
+      location: "Manti, Utah, United States",
+      dedicated: "1888, May, 21",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Payson Utah",
+      location: "Payson, Utah, United States",
+      dedicated: "2015, June, 7",
+      area: 96630,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Yigo Guam",
+      location: "Yigo, Guam",
+      dedicated: "2020, May, 2",
+      area: 6861,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Washington D.C.",
+      location: "Kensington, Maryland, United States",
+      dedicated: "1974, November, 19",
+      area: 156558,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Lima Perú",
+      location: "Lima, Perú",
+      dedicated: "1986, January, 10",
+      area: 9600,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+   {
+    templeName: "Portland Oregon",
+    location: "Portland, Oregon",
+    dedicated: "1989, August, 19-21",
+    area: "80500",
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/portland-oregon-temple/portland-oregon-temple-1629-main.jpg"
+   },
+   {
+    templeName: "San Diego Califonia",
+    location: "San Diego, California",
+    dedicated: "1993, April, 25-30",
+    area: "58005",
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california-temple/san-diego-california-temple-9060-main.jpg"
+   },
+   {
+    templeName: "Rio De Janeiro Brazil",
+    location: "Rio De Janeiro, Brazil",
+    dedicated: "2017, March, 4",
+    area: "29966",
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/rio-de-janeiro-brazil-temple/rio-de-janeiro-brazil-temple-8167-main.jpg"
+   },
+  ];
+createTempleCard();
+function createTempleCard() {
+    temples.forEach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement('h3');
+        let location = document.createElement('p');
+        let dedication = document.createElement('p');
+        let area = document.createElement('p');
+        let img = document.createElement('img');
+    
+        name.textContent = temple.templeNames;
+        location.innerHTML = '<span class= "label">Location</span> ${temple.location}';
+        dedication.innerHTML = '<span class= "label">Dedicated</span>${temple.dedicated}';
+        area.innerHTML = '<span class = "label">Size:</span> ${temple.area} sq ft';
+        img.setAttribute('src', temple.imageUrl);
+        img.setAttribute('alt', '${temple.templeName} Temple');
+        img.setAttribute('loading', "lazy");
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".res-grid").appendChild(card);
+});
+    }
